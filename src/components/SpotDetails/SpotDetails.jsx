78324a57f695cfbff45597c7fc73d6b124ad29f1
Camila Alm <camila.salmeidatech@hotmail.com>
2024-12-10 05:41:01 -0500

    commit before touching the spotdetails styling

--- src/components/SpotDetails/SpotDetails.jsx
+++ src/components/SpotDetails/SpotDetails.jsx
@@ -1,42 +1,32 @@
-// src/components/SpotDetails/SpotDetails.jsx
 
 import { useState, useEffect, useContext } from "react"
 import * as spotService from '../../services/spotService'
-                                                                       // This imports the useParams hook from react-router-dom. The useParams hook allows you to access the dynamic portions of the URL, known as route parameters. When your app uses routes like /spots/:spotId, the spotId part of the URL is dynamic and changes depending on the spot. useParams makes it easy to extract that dynamic value so you can use it within your component.
 import GuestForm from "../GuestForm/GuestForm";
-import { AuthedUserContext } from '../../App';                        // This makes the logged in user object easily accessible throughout our component tree.
+import { AuthedUserContext } from '../../App';                       
 import { useParams, useNavigate, Link } from "react-router-dom";
 import styles from './SpotDetails.module.css'
-
 
 const SpotDetails = (props) => {
     const [spot, setSpot] = useState(null)
     const user = useContext(AuthedUserContext);
-    //console.log('User CONTEXT:', user);
 
-    const { spotId } = useParams();                                    // Extracts the spotId parameter from the URL. If your route is /spots/:spotId, the value of :spotId in the URL will be assigned to the spotId variable.
-    // //console.log('spotId is: ', spotId)
+    const { spotId } = useParams();                           
 
-    useEffect(() => {                                                  // The useEffect hook in React runs a piece of code after the component renders. It allows you to perform side effects, such as fetching data or interacting with APIs. The useEffect will re-run only if the hootId value changes. If the hootId remains the same, the effect will not re-run. (Dependency Array([spotId]))
+    useEffect(() => {                                                  
         const fetchSpot = async () => {
             const spotData = await spotService.show(spotId)
-            //console.log('spotData is: ', spotData)
             setSpot(spotData)
         }
         fetchSpot()
-    }, [spotId])                                                        // Remember to include spotId in the dependency array of your useEffect(). This tells the useEffect() to fire off whenever the value of the spotId changes.
-    //console.log('spot state is: ', spot)
+    }, [spotId])                                                        
 
 const handleAddGuest = async (guestFormData) => {
-    //console.log('guestFormData is: ', guestFormData)
     const newGuest = await spotService.createGuest(spotId, guestFormData)
     setSpot({ ...spot, guests: [...spot.guests, newGuest] })
 }
 
 const handleDeleteGuest = async (guestId) => {
-    console.log('delete guest function is working and the guestId is:', guestId);
     const deletedGuest = await spotService.deleteGuest(spotId, guestId)
-    console.log('deletedGuest is: ', deletedGuest)
     setSpot({
         ...spot,
         guests: spot.guests.filter((guest) => guest._id !== guestId),
@@ -50,35 +40,43 @@ const handleResponse = async (guestId, status) => {
         guests: spot.guests.map((guest) =>
             guest._id === guestId ? { ...guest, status } : guest
         ),
-    });
-};
-
+    })
+}
 
 if (!spot) return <main>Loading...</main>
 return (
-        <main className={styles.con}> 
-            <header>
-                <p>{spot.category.toUpperCase()}</p>
-                <h1>{spot.spotName.toUpperCase()}</h1>
-                <p> <strong> 
-                    {spot.author?.username || 'Unknown'}</strong> created on {' '}
-                    {new Date(spot.createdAt).toLocaleDateString()} at {' '}
-                    {new Date(spot.createdAt).toLocaleTimeString()}
-                </p>
-                <p><strong>Address:</strong> {spot.address}</p>
-                <p><strong>Category:</strong> {spot.category}</p>
-                <p><strong>Dresscode:</strong> {spot.dresscode}</p>
+        <main className={styles.container}> 
+            <header className={styles.headerSection}>
+                                <h1>{spot.spotName.toUpperCase()}</h1>
+                                <p> <strong> 
+                                    {spot.author?.username || 'Unknown'}</strong> created on {' '}
+                                    {new Date(spot.createdAt).toLocaleDateString()} at {' '}
+                                    {new Date(spot.createdAt).toLocaleTimeString()}
+                                </p>
+                                <p><strong>Address:</strong> {spot.address}</p>
+                                <p><strong>Category:</strong> {spot.category}</p>
+                                <p><strong>Dresscode:</strong> {spot.dresscode}</p>
+                        {spot.author._id === user._id && (
 
-           
-           {spot.author._id === user._id && (
+
+
+
+
             <>
               <Link to={`/spots/${spotId}/edit`}>Edit</Link>
               <button onClick={() => props.handleDeleteSpot(spotId)}>Delete</button>
             </>
           )}
             </header>
-            <section>
 
+
+
+
+
+
+
+            
+            <section>
                 <h1>Guest Form</h1>
                 <GuestForm handleAddGuest={handleAddGuest}/>
 
@@ -116,8 +114,6 @@ return (
     )
 }
 
-//Ensures that if spot.author or user is null or undefined, it won’t throw an error. Instead, it will safely return undefined. {guest.author?.username || 'Unknown' }
 
 export default SpotDetails
 
-// <button onClick={() => props.handleDeleteGuest(guest._id)}>REMOVE GUEST</button>
